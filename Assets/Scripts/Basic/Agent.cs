2e2f7ebceacbd0b1c49856b40b77bfec3bd2fd22
elumixor <vladogim97@gmaill.com>
2020-05-09 15:33:18 +0200

    Sequential agent with discounted rewards

--- Assets/Scripts/Basic/Agent.cs
+++ Assets/Scripts/Basic/Agent.cs
@@ -1,18 +1,35 @@
-﻿using Num;
+﻿using System.Collections.Generic;
+using Num;
 using UnityEngine;
 using static F;
 
 namespace Basic {
     public class Agent : MonoBehaviour {
+        private float discount = 0.99f;
+        private float learningRate = 0.01f;
         private float b;
         public float P0 => b.Sigmoid();
         public int SampleAction => new System.Random().NextDouble() < P0 ? 0 : 1;
 
-        public void GetReward(int action, float reward) {
+        private float[] ToDiscounted(IReadOnlyList<float> rewards) {
+            var cumulativeRewards = 0f;
+            var discounted = new float[rewards.Count];
+
+            for (var i = rewards.Count - 1; i >= 0; i--)
+                discounted[i] = cumulativeRewards = cumulativeRewards * discount + rewards[i];
+
+            return discounted;
+        }
+
+        private void Learn(int action, float reward) {
             var db = b.Sigmoid() * (1 - b.Sigmoid());
             if (action == 1) db = -db;
-            Debug.Log(db);
-            b += db * 0.01f * reward;
+            b += db * learningRate * reward;
+        }
+
+        public void Learn(List<int> actions, List<float> rewards) {
+            var r = ToDiscounted(rewards);
+            for (var i = 0; i < r.Length; i++) Learn(actions[i], rewards[i]);
         }
 
         public void ResetBrain() {
--- Assets/Scripts/Basic/Environment.cs
+++ Assets/Scripts/Basic/Environment.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Basic {
@@ -11,6 +13,7 @@ namespace Basic {
 
         [SerializeField] private bool automatic;
         [SerializeField] private int maxEpisodes;
+        [SerializeField] private int maxSamples;
 
         private int episodes;
 
@@ -18,24 +21,51 @@ namespace Basic {
             ResetEnvironment();
         }
 
+        private List<int> actions;
+        private List<float> rewards;
+
         public void Step() {
             var p0 = agent.P0;
             visualizer.SetValue(p0);
+
             var action = agent.SampleAction;
-            var reward = action == 0 ? smallReward : bigReward;
-            agent.GetReward(action, reward);
-            Debug.Log(action);
+            print(action);
+            actions.Add(action);
+
+            if (action == 0) agent.transform.Translate(-1f, 0f, 0f);
+            else agent.transform.Translate(1f, 0f, 0f);
+
+            var x = agent.transform.localPosition.x;
+            var terminalState = Mathf.Abs(x) > 4.5f;
+
+            var reward = (float) (terminalState ? (x > 4.5f ? bigReward : smallReward) : -0.1);
+            rewards.Add(reward);
+
+            agent.Learn(actions, rewards);
+
+            if (terminalState || rewards.Count >= maxSamples) {
+                print($"Episode {episodes}. Total Reward: {rewards.Sum()}");
+                NewEpisode();
+            }
+        }
+
+        private void NewEpisode() {
+            agent.transform.localPosition = Vector3.zero;
+            actions = new List<int>();
+            rewards = new List<float>();
 
             episodes += 1;
-            if (episodes >= maxEpisodes) {
-                ResetEnvironment();
-            }
+            // if (episodes >= maxEpisodes) {
+            //     ResetEnvironment();
+            //     return;
+            // }
         }
 
         public void ResetEnvironment() {
             episodes = 0;
             agent.ResetBrain();
             visualizer.SetValue(agent.P0);
+            NewEpisode();
         }
 
         private int t = 0;
